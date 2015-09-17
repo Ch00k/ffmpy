03d1ff62d2ca811bbba85ac8f56a3f3503fc4dba
Andriy Yurchuk <ay@mntw.re>
2015-09-17 21:24:29 +0200

    Add ordereddict implementation for Python 2.6

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,14 @@
-from collections import OrderedDict
+import sys
 
 from ffmpy import FF
+
+PY2 = int(sys.version[0]) == 2
+PY26 = PY2 and int(sys.version_info[1]) < 7
+
+if PY26:
+    from .ordereddict import OrderedDict
+else:
+    from collections import OrderedDict
 
 
 def test_default():
